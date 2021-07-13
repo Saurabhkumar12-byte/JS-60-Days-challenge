53e50affee069d71358092679fa0aef96d6fb54e
Saurabh <saurabhsingh4778698@gmail.com>
2021-07-13 17:16:05 +0530

    first commit

---
+++ Day1/script.js
@@ -0,0 +1,14 @@
+const color = document.getElementById("color-name");
+const btn = document.getElementById("btn");
+const box = document.querySelector(".box");
+const button = document.querySelector("button");
+
+const colorArr = ["#32de84", "#00308F", "#E52B50", "#FEBE10"];
+
+btn.addEventListener("click", () => {
+  index = Math.floor(Math.random() * 4);
+  box.style.backgroundColor = colorArr[index];
+  color.style.backgroundColor = colorArr[index];
+  button.style.backgroundColor = colorArr[index];
+  color.innerHTML = colorArr[index];
+});
